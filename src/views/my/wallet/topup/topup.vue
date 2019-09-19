09274edcc5aa116aa79bd503b8e5b42aca41391a
李利军 <13587611202@koall.io>
2019-09-19 18:32:21 +0800

    EOS 和 XRP的充提页面MEMO修改
    Signed-off-by: 李利军 <13587611202@koall.io>

--- src/views/my/wallet/topup/topup.vue
+++ src/views/my/wallet/topup/topup.vue
@@ -13,14 +13,22 @@
                         <!--.使用BCH地址充值需要6个网格确认才能到账，具体到账以实际到账时间为准--></li>
                     <li>2. {{$t('account.user_prompt2').format(symbolInfo.symbol)}}
                         <!--充值最小额度为0.01BCH,小于0.01BCH将无法到账。--></li>
+                    <li style="color: #E14B26; " v-if="symbol==='EOS' || symbol==='XRP'">3. {{$t('account.userRechargeMemoTip')}}
+                        <!-- Memo和Address需要同时使用才能充值成功！ --></li>
                 </ul>
             </div>
             <section class="wallet-contant">
                 <div class="item">
                     <p class="title">{{symbolInfo.symbol}}{{$t('referral.address')}}</p>
-                    <div ref="qrcode" class="qrcode" id="picture"></div>
-                    <p class="two f26">{{symbolInfo.address}}</p>
+                    <div ref="qrcode" class="qrcode"></div>
+                    <p class="two f26">{{getAddress}}</p>
                     <p class="three" v-tap="{methods:copy}">{{$t('referral.copy')}}<!--复制--></p>
+                </div>
+                <div class="item"  v-if="symbol==='EOS' || symbol==='XRP'">
+                    <p class="title">{{symbol}} {{$t('account.recharge_memo')}}<!--充值备注--></p>
+                    <div ref="memoQrcode" class="qrcode"></div>
+                    <p class="two f26">{{symbolInfo.address}}</p>
+                    <p class="three" v-tap="{methods:copyMemo}">{{$t('referral.copy')}}<!--复制--></p>
                 </div>
             </section>
         </div>
@@ -39,18 +47,41 @@
         data() {
             return {
                 symbol: '',
-                symbolInfo: {}
+                symbolInfo: {},
+                EOS_MEMO:''
             }
         },
         computed: {
-            ...mapGetters(['getUserWallets', 'getSymbol']),
+            ...mapGetters(['getUserWallets', 'getSymbol', 'getSysParams']),
+            XRP_MEMO(){
+              return (this.getSysParams['mainAddXRP'] && this.getSysParams.mainAddXRP.value) || ''
+            },
+            getAddress(){
+              return this.symbol==='EOS'?this.EOS_MEMO:(this.symbol==='XRP'?this.XRP_MEMO:this.symbolInfo.address)
+            }
         },
-        watch: {},
+        watch: {
+            getAddress () {
+              utils.qrcode(this.$refs.qrcode, {
+                  text: this.getAddress,
+                  width: 150,
+                  height: 150
+              })
+            },
+        },
         created() {
             this.symbol = this.getSymbol
             this.getInfo()
+            if(this.symbol==='EOS' && !this.EOS_MEMO){
+              this.getEosAddress()
+            }
             this.$nextTick(() => {
                     utils.qrcode(this.$refs.qrcode, {
+                        text: this.getAddress,
+                        width: 150,
+                        height: 150
+                    })
+                    utils.qrcode(this.$refs.memoQrcode, {
                         text: this.symbolInfo.address,
                         width: 150,
                         height: 150
@@ -60,6 +91,11 @@
             console.log(this.getUserWallets,this.getSymbol)
         },
         methods: {
+            getEosAddress(){
+              userUtils.getEosAddress(data=>{
+                this.EOS_MEMO = data
+              })
+            },
             getInfo() {
                 this.getUserWallets.filter(res => {
                     if (this.getSymbol === res.symbol) {
@@ -68,6 +104,10 @@
                 })
             },
             copy() {
+                utils.copyText(this.getAddress)
+                Toast(this.$t('public0.public33')) // 复制成功
+            },
+            copyMemo() {
                 utils.copyText(this.symbolInfo.address)
                 Toast(this.$t('public0.public33')) // 复制成功
             },
