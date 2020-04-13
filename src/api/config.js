03e9218f4583709aacf92096b9d230998a0c11d6
fla126 <fla126@qq.com>
2020-04-13 17:54:04 +0800

    修改更新日志
    Signed-off-by: fla126 <fla126@qq.com>

--- src/api/config.js
+++ src/api/config.js
@@ -20,13 +20,13 @@ const config = {
   version:'1.1.1',
   updateInfo:{  //更新日志
   	'zh-CN':`
-	<p>1. 优化kline<br>2. 优化交易市场显示</p>
+	<p>1. 优化交易页面深度显示<br>2. 优化提现记录详情时间显示</p>
 	`,
 	'en':`
-	<p>1. Optimize kline<br>2. Optimize trading market display</p>
+	<p>1. Optimize transaction page depth display<br>2. Optimize the time display of withdrawal record details</p>
 	`,
 	'kr':`
-	<p>1. 최적화된 kline<br> 2. 거래시장최적화결과</p>
+	<p>1. 거래 페이지 깊이 표시 최적화<br> 2. 현금 인출 기록 상세 시간 표시 최적화</p>
 	`
   }
 }
