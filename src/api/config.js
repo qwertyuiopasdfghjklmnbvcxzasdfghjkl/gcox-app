c8eaf8f9667703ea574f2c0f204493741bfb5e0a
fla126 <fla126@qq.com>
2020-10-12 14:51:27 +0800

    添加更新日志
    Signed-off-by: fla126 <fla126@qq.com>

--- src/api/config.js
+++ src/api/config.js
@@ -23,13 +23,13 @@ const config = {
   version:'1.1.1',
   updateInfo:{  //更新日志
   	'zh-CN':`
-	<p>1. 优化交易页面深度显示<br>2. 优化提现记录详情时间显示</p>
+	<p>1. 优化用户中心钱包资产显示</p>
 	`,
 	'en':`
-	<p>1. Optimize transaction page depth display<br>2. Optimize the time display of withdrawal record details</p>
+	<p>1. Optimize the display of wallet assets in user center</p>
 	`,
 	'kr':`
-	<p>1. 거래 페이지 깊이 표시 최적화<br> 2. 현금 인출 기록 상세 시간 표시 최적화</p>
+	<p>1. 사용자 센터 지갑 자산 표시 최적화</p>
 	`
   }
 }
